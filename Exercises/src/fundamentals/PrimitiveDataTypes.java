74299361ed2e8a9226ee7e4bac42059700f0b577
Paulo Araújo <paulojosejr95@gmail.com>
2022-10-14 09:43:51 +0100

    Primitive data types

---
+++ Exercises/src/fundamentals/PrimitiveDataTypes.java
@@ -0,0 +1,27 @@
+package fundamentals;
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		//integer
+		byte yearsOfComprany = 12;
+		short numberOfFlights = 542;
+		int id = 56789;
+		long accumulatedPoints = 3_234_845_223L;
+		
+		//real
+		float salary = 11_445.44F;
+		double accumulatedSales = 3_991_797_103.01;
+		
+		boolean isOnVacation = false;
+		
+		char status = 'A';
+		
+		System.out.println("Employee ID: " + id);
+		System.out.println("Marcos has " + yearsOfComprany + "years of comprany");
+		System.out.println("Flights: " + numberOfFlights);
+		System.out.println("Accumulated Points: " + accumulatedPoints);
+		System.out.println("Salary: " + salary);
+		System.out.println("Is on Vacation: " + isOnVacation);
+		System.out.println("Status: " + status);
+	}
+}
