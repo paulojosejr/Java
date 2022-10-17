830cb80dde661a7dceb80d2a39514e1a7db51b4b
Paulo Araújo <paulojosejr95@gmail.com>
2022-10-17 17:25:08 +0100

    Difference between primitives and objects

--- Exercises/src/fundamentals/PrimitiveVsObject.java
+++ Exercises/src/fundamentals/PrimitiveVsObject.java
@@ -1,5 +1,13 @@
 package fundamentals;
 
 public class PrimitiveVsObject {
-
+	public static void main(String[] args) {
+		
+		String s = new String("text");
+		s.toUpperCase();
+		
+		// Wrappers
+		int a = 123;
+		System.out.println(a);
+	}
 }
