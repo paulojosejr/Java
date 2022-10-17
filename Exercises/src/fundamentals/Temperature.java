d5aaa90465ba0913a417bdb67f1a47ae59b49452
Paulo Araújo <paulojosejr95@gmail.com>
2022-10-17 17:24:26 +0100

    difference between primitive and objects

--- Exercises/src/fundamentals/Temperature.java
+++ Exercises/src/fundamentals/Temperature.java
@@ -6,11 +6,11 @@ public class Temperature {
 	public static void main(String[] args) {
 		final double ADJUSTMENT = 32;
 		final double FACTOR = 5.0/9.0;
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Enter the temperature in Fahrenheit");
-			int fahrenheit = sc.nextInt();
-			double celsius = (fahrenheit - ADJUSTMENT) * FACTOR;
-			System.out.println("Fahrenheit = " + fahrenheit + " | Celsius = " + celsius);
-		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the temperature in Fahrenheit");
+		int fahrenheit = sc.nextInt();
+		double celsius = (fahrenheit - ADJUSTMENT) * FACTOR;
+		System.out.println("Fahrenheit = " + fahrenheit + " | Celsius = " + celsius);
+		sc.close();
 	}
 }
