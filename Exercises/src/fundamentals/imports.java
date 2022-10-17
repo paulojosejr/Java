2ca5694909c80d528b15d53a6b6f1090ecf45103
Paulo Araújo <paulojosejr95@gmail.com>
2022-10-17 16:59:31 +0100

    imports in java

---
+++ Exercises/src/fundamentals/imports.java
@@ -0,0 +1,15 @@
+package fundamentals;
+import java.util.Date;
+//import javax.swing.JButton;
+
+public class imports {
+	public static void main(String[] args) {
+		java.lang.String b = "Good evening";
+		System.out.println(b);
+		
+		Date dt = new Date();
+		System.out.println(dt);
+		
+		//JButton btn = new JButton();
+	}
+}
