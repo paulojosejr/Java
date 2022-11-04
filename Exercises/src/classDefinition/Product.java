48e1fc2f2d5f7858f114ecda8ee6eb98a55913cb
Paulo Araújo <paulojosejr95@gmail.com>
2022-11-04 09:41:01 +0000

    add constructors in to product

--- Exercises/src/classDefinition/Product.java
+++ Exercises/src/classDefinition/Product.java
@@ -4,12 +4,26 @@ public class Product {
 	String name;
 	double price;
 	double discount;
-	
+
+	Product() {
+
+	}
+
+	Product(String startName) {
+		name = startName;
+	}
+
+	Product(String startName, double startPrice, double startDiscount) {
+		name = startName;
+		price = startPrice;
+		discount = startDiscount;
+	}
+
 	double discountPrice(double extraDiscount) {
 		double result = price * (1 - (discount + extraDiscount));
 		return result;
 	}
-	
+
 	double discountPrice() {
 		double result = price * (1 - discount);
 		return result;
--- Exercises/src/classDefinition/TestProduct.java
+++ Exercises/src/classDefinition/TestProduct.java
@@ -3,13 +3,10 @@ package classDefinition;
 public class TestProduct {
 	public static void main(String[] args) {
 		
-		Product prod1 = new Product();
-		prod1.name = "Notebook";
-		prod1.price = 2399.50;
-		prod1.discount = 0.3;
+		Product prod1 = new Product("Notebook", 2399.50, 0.3);
 		
 		var prod2 = new Product();
-		prod2.name = "black pen";
+		prod2.name = "Black pen";
 		prod2.price = 12.56;
 		prod2.discount = 0.25;
 		
