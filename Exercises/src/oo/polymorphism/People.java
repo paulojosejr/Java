763782c99132420459f92da623a559efac8fd707
Paulo Araújo <paulojosejr95@gmail.com>
2022-12-12 09:34:50 +0000

    add dinner class and add methods in people

--- Exercises/src/oo/polymorphism/People.java
+++ Exercises/src/oo/polymorphism/People.java
@@ -1,5 +1,31 @@
 package oo.polymorphism;
 
 public class People {
+	private double weight;
+	
+	public double getWeight() {
+		return weight;
+	}
 
+	public void eat(Rice rice) {
+		this.weight += rice.getWeight();
+	}
+	
+	public void eat(Bean bean) {
+		this.weight += bean.getWeight();
+	}
+	
+	public void eat(IceCream iceCream) {
+		this.weight += iceCream.getWeight();
+	}
+	
+	public void setWeight(double weight) {
+		if(weight >= 0) {
+			this.weight = weight;
+		}
+	}
+	
+	public People (double weight) {
+		this.weight = weight;
+	}
 }
