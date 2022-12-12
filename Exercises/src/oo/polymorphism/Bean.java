2959c626f3f52b5f12b2fbcc084b93be19caa153
Paulo Araújo <paulojosejr95@gmail.com>
2022-12-12 09:31:06 +0000

    add constructor in bean

--- Exercises/src/oo/polymorphism/Bean.java
+++ Exercises/src/oo/polymorphism/Bean.java
@@ -1,5 +1,19 @@
 package oo.polymorphism;
 
 public class Bean {
+	private double weight;
+	
+	public double getWeight() {
+		return weight;
+	}
 
+	public void setWeight(double weight) {
+		if(weight >= 0) {
+			this.weight = weight;
+		}
+	}
+	
+	public Bean (double weight) {
+		this.weight = weight;
+	}
 }
