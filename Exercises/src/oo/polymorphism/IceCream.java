e5e825c5463c033a3b32df8d0ee5f4d6f64eac44
Paulo Araújo <paulojosejr95@gmail.com>
2022-12-12 09:29:05 +0000

    add constructor in icecream

--- Exercises/src/oo/polymorphism/IceCream.java
+++ Exercises/src/oo/polymorphism/IceCream.java
@@ -1,5 +1,20 @@
 package oo.polymorphism;
 
 public class IceCream {
+	private double weight;
+	
+	public double getWeight() {
+		return weight;
+	}
 
+	public void setWeight(double weight) {
+		if(weight >= 0) {
+			this.weight = weight;
+		}
+	}
+	
+	public IceCream (double weight) {
+		this.weight = weight;
+	}
+	
 }
