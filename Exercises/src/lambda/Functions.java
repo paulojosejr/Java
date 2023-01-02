5056b17b752a7dcdd3654c81f29ae0078df2a4f8
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-02 10:09:59 +0000

    create a function to check if is even or odd

--- Exercises/src/lambda/Functions.java
+++ Exercises/src/lambda/Functions.java
@@ -1,5 +1,11 @@
 package lambda;
 
-public class Functions {
+import java.util.function.Function;
 
+public class Functions {
+	public static void main(String[] args) {
+		
+		Function<Integer, String> evenOrOdd = number -> number % 2 == 0 ? "Even" : "Odd";
+		
+	}
 }
