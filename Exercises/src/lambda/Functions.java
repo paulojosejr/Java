78079319678fbae8967626f7e38ae4d0b4b788d1
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-02 10:22:42 +0000

    create nested functions

--- Exercises/src/lambda/Functions.java
+++ Exercises/src/lambda/Functions.java
@@ -7,5 +7,18 @@ public class Functions {
 		
 		Function<Integer, String> evenOrOdd = number -> number % 2 == 0 ? "Even" : "Odd";
 		
+		Function<String, String> result= value -> "The result is: " + value;
+		
+		Function<String, String> excited = value -> value + "!!!";
+		
+		Function<String, String> doubt = value -> value + "???";
+		
+		String finalResult1 = evenOrOdd.andThen(result).andThen(excited).apply(32);
+		System.out.println(finalResult1);
+		
+		String finalResult2 = evenOrOdd.andThen(result).andThen(doubt).apply(33);
+		System.out.println(finalResult2);
+		
+		System.out.println(evenOrOdd.apply(32));
 	}
 }
