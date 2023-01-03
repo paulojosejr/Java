6fed772215de88ae55b4b647f3f126e0caef7174
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-03 10:01:11 +0000

    Update Suppliers.java

--- Exercises/src/lambda/Suppliers.java
+++ Exercises/src/lambda/Suppliers.java
@@ -1,5 +1,14 @@
 package lambda;
 
-public class Suppliers {
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
 
+public class Suppliers {
+	public static void main(String[] args) {
+		
+		Supplier<List<String>> listOne = () -> Arrays.asList("Paulo", "Juliana", "Lucas");
+		
+		System.out.println(listOne.get());
+	}
 }
