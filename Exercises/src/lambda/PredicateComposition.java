442aaed12d493d84be082f7eafe0258146575a3a
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-03 09:53:13 +0000

    Update PredicateComposition.java

--- Exercises/src/lambda/PredicateComposition.java
+++ Exercises/src/lambda/PredicateComposition.java
@@ -1,5 +1,14 @@
 package lambda;
 
-public class PredicateComposition {
+import java.util.function.Predicate;
 
+public class PredicateComposition {
+	public static void main(String[] args) {
+		
+		Predicate<Integer> isEven = num -> num % 2 == 0;
+		Predicate<Integer> isThreeDigits = num -> num >= 100 && num <= 999;
+		
+		System.out.println(isEven.and(isThreeDigits).negate().test(123));
+		System.out.println(isEven.or(isThreeDigits).test(123));
+	}
 }
