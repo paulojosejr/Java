705cd54ed4fac990d1aeba4cd6295dfa6a32a59b
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-06 09:57:01 +0000

    Update printingObjects.java

--- Exercises/src/streams/printingObjects.java
+++ Exercises/src/streams/printingObjects.java
@@ -1,5 +1,29 @@
 package streams;
 
-public class printingObjects {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
 
+public class printingObjects {
+	public static void main(String[] args) {
+		
+		List<String> approveds = Arrays.asList("Juliana", "Paulo", "Jon", "Rick");
+		
+		System.out.println("With foreach");
+		for(String name: approveds) {
+			System.out.println(name);
+		}
+		
+		System.out.println("\nWith Iterator");
+		
+		Iterator<String> iterator = approveds.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("\nWith Stream");
+		Stream<String> stream = approveds.stream();
+		stream.forEach(System.out::println);
+	}
 }
