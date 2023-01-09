fe33de06e758fde9e1966e806e887d3514fbf7c0
Paulo Araújo <paulojosejr95@gmail.com>
2023-01-09 11:16:50 +0000

    initial commit

---
+++ encrypt-user-password/src/test/java/javaproject/encryptuserpassword/EncryptUserPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package javaproject.encryptuserpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptUserPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
